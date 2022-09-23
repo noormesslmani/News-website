d7b7d02a6fb6ba0bda657f96e0ba441cbd646d8c
noormesslmani <noor.messlmani@gmail.com>
2022-09-23 20:46:10 +0300

    created apis and pages for several types of news

---
+++ backend/selectpolitics.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+include("connection.php");
+
+$query = $mysqli->prepare("SELECT id, title, contents, time from news,type where
+news.type_id_type= type.id_type and type.type='politics' ");
+$query->execute();
+$array = $query->get_result();
+
+$response = [];
+
+while($a = $array->fetch_assoc()){  
+    $response[] = $a;
+}
+
+echo json_encode($response);
+
+?>
--- frontend/JS/health.js
+++ frontend/JS/health.js
@@ -1,10 +1,15 @@
-const news= document.getElementById('news')
+const newsHealth= document.getElementById('healthnews')
 
-let newssection
+let newssection= `<div class="col-lg-4">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  </div>`
 $(document).ready(() => {
     $.ajax({
         type: "GET",
-        url: "http://localhost/news/backend/selectnews.php",
+        url: "http://localhost/news/backend/health.php",
         dataType: "html",
         success: (json) => {
         let data = JSON.parse(json);
@@ -12,11 +17,11 @@ $(document).ready(() => {
             console.log(newsdetails.time)
             newssection= `<div class="col-lg-4 news ">
             <div class="title">${newsdetails.title}</div>
-            <div class="type">${newsdetails.type}</div>
+            <div class="type">Health</div>
             <div class="contents">${newsdetails.contents}</div>
             <div class="date">${newsdetails.time}</div>
             </div>` 
-        news.innerHTML+= newssection}
+        newsHealth.innerHTML+= newssection}
         console.log(data);}
     })
 })
